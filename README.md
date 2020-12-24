5061d65679cecd14e720c9fbfea5f4b0d7dd4677
m_cre <mc@mchs-u.net>
2020-12-25 01:03:40 +0900

    configに入出力パス、テンプレート、処理内容を記載できるようにした

--- README.md
+++ README.md
@@ -2,6 +2,7 @@ address-printing-on-jp-postcards
 =================================
 
 はがきの宛名印刷のために、csvの住所録をPDFに変換します。
+configやテンプレートを変更することで、柔軟な入出力が可能です。
 
 ## Requirements
 
@@ -15,16 +16,63 @@ cd address-printing-on-jp-postcards
 docker build -t texlive-python .
 ```
 
-サンプル実行
+### サンプルの実行
 
 ```
-docker run -it --rm -v $(pwd):/workdir --rm texlive-python sh -c "python3 main.py input/sample01.csv config/sample01.json"
-docker run -it --rm -v $(pwd):/workdir --rm texlive-python sh -c "python3 main.py input/sample02.csv config/sample02.json"
+docker run -it --rm -v $(pwd):/workdir --rm texlive-python sh -c "python3 main.py config/sample01.json"
+docker run -it --rm -v $(pwd):/workdir --rm texlive-python sh -c "python3 main.py config/sample02.json"
 ```
+
+### カスタマイズ
+
+1. [input/](input/) に住所録のcsvファイルを配置します。
+2. サンプル([1](config/sample01.json), [2](config/sample02.json))と下記configのフォーマット説明を参考にconfigを作成してください。
+3. 必要に応じて [templates/](templates/) も書き換えます。
+4. 下記のコードで実行します。
+
+```
+docker run -it --rm -v $(pwd):/workdir --rm texlive-python sh -c "python3 main.py config/{ファイル名}.json"
+```
+
+## configのフォーマット説明
+
+* input
+  - 入力csvファイルのパス
+* output
+  - 出力pdfファイルのパス
+* templates: LuaLaTeX形式のテンプレートファイル
+  - document
+    - ドキュメント全体のフォーマットを設定したファイルです。
+    - pageを埋め込む部分には`@{pages}`を記載する必要があります。
+    - pages以外の変数の埋め込みはできません。
+  - page
+    - 各ページ(1枚のはがき)のフォーマットを設定したファイルです。
+    - `@{hoge}`の形式でcsvの各行の項目を埋め込むことができます。
+  - page_break
+    - ページの区切りに入る行を記載したファイルです。
+    - 変数の埋め込みはできません。
+* csv_format
+  - `"tex埋め込み変数名": "csvの項目名"`を記載します。
+  - "csvの項目名"はリストで複数指定することができます。その場合各項目がスペースなしで並びます。
+    - 例: `["都道府県", "市区町村", "町域", "番地"]`
+* processing
+  - csvの項目に下記の処理を行います。それぞれリスト形式で複数設定できます。下記の順で処理します。
+    - print_only_then
+      - `col`が`value`と等しいときのみ、そのページを出力します。
+    - dont_print_then
+      - `col`が`value`と等しいときは、そのページを出力しません。
+    - replace
+      - `col`の文字列のうち`from`の文字を`to`に置換します。
+    - split
+      - `col`の文字列を1文字ずつに分解し`{col}_XX`のtex埋め込み変数を生成します。XXはゼロから始まるゼロ埋めの2桁の数値です。
+    - default
+      - `col`がcsvで空文字列の場合または指定されていない場合に、`col`に`value`を設定します。
+    - show_only_if_exists
+      - `exists`がcsvで空文字列の場合または指定されていない場合に、`col`に空文字列を設定します。
 
 ## Acknowledgments
 
-* texファイルの構成等はこちらのリポジトリを参考にさせていただきました。
+* texファイルに一部はこちらのリポジトリのソースコードを使用させていただいています
   - https://github.com/rago1975/atena-sashikomi
 
 ## Contacts
--- main.py
+++ main.py
@@ -13,7 +13,47 @@ class Template(string.Template):
     delimiter = '@'
 
 
-def main(input_file, config_file):
+def read(path):
+    with open(path, 'r', encoding='utf-8') as f:
+        return f.read()
+
+
+def write(path, text):
+    with open(path, 'w', encoding='utf-8') as f:
+        f.write(text)
+
+
+def processing(pconf, address):
+    for p in pconf.get('print_only_then', []):
+        if address[p['col']] != p['value']:
+            return None
+
+    for p in pconf.get('dont_print_then', []):
+        if address[p['col']] == p['value']:
+            return None
+
+    for p in pconf.get('replace', []):
+        address[p['col']] = address[p['col']].replace(p['from'], p['to'])
+
+    for p in pconf.get('split', []):
+        for i, v in enumerate(address[p['col']]):
+            address[f'{p["col"]}_{i:02}'] = v
+
+    for p in pconf.get('default', []):
+        if address.get(p['col']) == '':
+            address[p['col']] = p['value']
+
+    for p in pconf.get('show_only_if_exists', []):
+        if address.get(p['exists'], '') == '':
+            address[p['col']] = ''
+
+    for k, v in address.items():
+        address[k] = jaconv.h2z(v, ascii=True, digit=True)
+
+    return address
+
+
+def main(config_file):
     os.makedirs('work', exist_ok=True)
     os.makedirs('output', exist_ok=True)
 
@@ -21,7 +61,7 @@ def main(input_file, config_file):
         config = json.load(f)
 
     address_list = []
-    with open(input_file, 'r', encoding='utf-8') as f:
+    with open(config['input'], 'r', encoding='utf-8') as f:
         for row in csv.DictReader(f):
             address = {}
             for k, v in config['csv_format'].items():
@@ -29,47 +69,32 @@ def main(input_file, config_file):
                     address[k] = row.get(v, '')
                 else:
                     address[k] = ''.join([row.get(vi, '') for vi in v])
-                address[k] = jaconv.h2z(address[k], ascii=True, digit=True)
-            for i in range(4):
-                fn = f'first_name_{i + 1}'
-                ht = f'honorific_title_{i + 1}'
-                if address.get(fn, '') != '' and address.get(ht, '') == '':
-                    address[ht] = '様'
-            for i in range(7):
-                address[f'pc{i + 1}'] = address['postal_code'].replace('－', '')[i]
 
-            address_list.append(address)
+            address = processing(config['processing'], address)
+            if address is not None:
+                address_list.append(address)
 
-    with open('templates/page.tex', 'r', encoding='utf-8') as f:
-        page_template = f.read()
+    doc_template = Template(read(config['templates']['document']))
+    page_template = Template(read(config['templates']['page']))
+    break_tex = read(config['templates']['page_break'])
 
-    pages_tex = ''
+    pages = []
     for address in address_list:
-        pages_tex += Template(page_template).substitute(address) + '\\newpage\n'
+        pages.append(page_template.substitute(address))
 
-    with open('templates/document.tex', 'r', encoding='utf-8') as f:
-        doc_template = f.read()
+    tex = doc_template.substitute({'pages': f'\n{break_tex}\n'.join(pages)})
 
-    tex = Template(doc_template).substitute({'pages': pages_tex})
-    with open('work/tmp.tex', 'w', encoding='utf-8') as f:
-        f.write(tex)
-
+    t = 'work/tmp.tex'
+    write(t, tex)
     try:
-        subprocess.run('lualatex --halt-on-error --output-directory work work/tmp.tex > tex_output.txt', shell=True, check=True)
+        subprocess.run(f'lualatex --halt-on-error --output-directory work {t} > work/tex_output.txt', shell=True, check=True)
     except subprocess.CalledProcessError:
         print('luatexでエラーが発生しました。')
     else:
-        output = f'output/{os.path.basename(input_file)}.pdf'
+        output = config['output']
         shutil.copy('work/tmp.pdf', output)
         print('PDFを出力しました:', output)
 
 
 if __name__ == '__main__':
-    input_file = 'input/sample01.csv'
-    config_file = 'config/sample01.json'
-    ln = len(sys.argv)
-    if ln > 1:
-        input_file = sys.argv[1]
-    if ln > 2:
-        config_file = sys.argv[2]
-    main(input_file, config_file)
+    main(sys.argv[1] if len(sys.argv) > 1 else 'config/sample01.json')
